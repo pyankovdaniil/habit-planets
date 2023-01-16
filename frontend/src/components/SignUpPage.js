62790765dee5451c1781404ee71c85a4f17eb8d2
Pyankov Daniil <pyankovdaniil@yandex.ru>
2023-01-16 19:16:35 +0300

    added styles for registration and authentication pages

--- frontend/src/components/SignUpPage.js
+++ frontend/src/components/SignUpPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useForm} from 'react-hook-form';
 import TextField from '@mui/material/TextField';
 import { Box, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function SignUpPage() {
   const fullNameMinLength = 3;
@@ -47,126 +48,138 @@ function SignUpPage() {
 
   return (
     <div className="SignUpPage">
-      <Box id="sign-up-form-box">
-        <Box class="centered">
-          <h1 id="sign-up-welcome-message">
-            Зарегистрируйтесь на Habit Planets!
-          </h1>
+      <Box id="sign-up-main-container">
+        <Box id="sign-up-info-box">
+          <img src="../images/planets/TERRAIN_WET.png" id="sign-up-rotating-planet" alt="Spinning planet"></img>
         </Box>
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Box class="sign-up-field-box centered">
-            <TextField size="small" fullWidth id="fullName" label="Как вас называть?"
-              variant="outlined" color="secondary" {...register("fullName", {
-                required: "Обязательно введите имя!",
-                minLength: {
-                  value: fullNameMinLength,
-                  message: `Минимальная длина имени - ${fullNameMinLength} символов!`
-                },
-                maxLength: {
-                  value: fullNameMaxLength,
-                  message: `Максимальная длина имени - ${fullNameMaxLength} символов!`
-                }
-              })}/>
-          </Box>
-
-          <Box class="sign-up-field-error-box centered">
-            {
-              errors?.fullName &&
-              <p class="sign-up-field-error-message">
-                {errors?.fullName?.message || "Ошибка валидации имени!"}
+        <Box id="sign-up-form-box">
+          <Box>
+            <h1 class="centered" id="sign-up-welcome-message">
+              Создайте аккаунт
+              <p id="sign-up-redirect-message">
+                <Link id="sign-in-form-link" to="/sign-in">
+                  Войти
+                </Link>
+                вместо этого?
               </p>
-            }
+            </h1>
           </Box>
 
-          <Box class="sign-up-field-box centered">
-            <TextField size="small" fullWidth id="email" label="Электронная почта"
-              variant="outlined" color="secondary" {...register("email", {
-                required: "Обязательно введите email!",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
-                  message: "Введите корректный email!"
-                },
-                minLength: {
-                  value: emailMinLength,
-                  message: `Минимальная длина почты - ${emailMinLength} символов!`
-                },
-                maxLength: {
-                  value: emailMaxLength,
-                  message: `Максимальная длина почты - ${emailMaxLength} символов!`
-                }
-              })}/>
-          </Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box class="sign-up-field-box centered">
+              <TextField size="small" fullWidth id="fullName" label="Как вас называть?"
+                variant="outlined" color="secondary" {...register("fullName", {
+                  required: "Обязательно введите имя!",
+                  minLength: {
+                    value: fullNameMinLength,
+                    message: `Минимальная длина имени - ${fullNameMinLength} символов!`
+                  },
+                  maxLength: {
+                    value: fullNameMaxLength,
+                    message: `Максимальная длина имени - ${fullNameMaxLength} символов!`
+                  }
+                })}/>
+            </Box>
 
-          <Box class="sign-up-field-error-box centered">
-            {
-              errors?.email &&
-              <p class="sign-up-field-error-message">
-                {errors?.email?.message || "Ошибка валидации почты!"}
-              </p>
-            }
-          </Box>
+            <Box class="sign-up-field-error-box centered">
+              {
+                errors?.fullName &&
+                <p class="sign-up-field-error-message">
+                  {errors?.fullName?.message || "Ошибка валидации имени!"}
+                </p>
+              }
+            </Box>
 
-          <Box class="sign-up-field-box centered">
-            <TextField size="small" fullWidth id="username" label="Юзернейм"
-              variant="outlined" color="secondary" {...register("username", {
-                required: "Обязательно введите юзернейм!",
-                validate: value => isUsernameUnique(value),
-                pattern: {
-                  value: /^[a-z0-9_-]*$/,
-                  message: "Введите корректный юзернейм!"
-                },
-                minLength: {
-                  value: usernameMinLength,
-                  message: `Минимальная длина юзернейма - ${usernameMinLength} символов!`
-                },
-                maxLength: {
-                  value: usernameMaxLength,
-                  message: `Максимальная длина юзернейма - ${usernameMaxLength} символов!`
-                }
-              })}/>
-          </Box>
+            <Box class="sign-up-field-box centered">
+              <TextField size="small" fullWidth id="email" label="Электронная почта"
+                variant="outlined" color="secondary" {...register("email", {
+                  required: "Обязательно введите email!",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+                    message: "Введите корректный email!"
+                  },
+                  minLength: {
+                    value: emailMinLength,
+                    message: `Минимальная длина почты - ${emailMinLength} символов!`
+                  },
+                  maxLength: {
+                    value: emailMaxLength,
+                    message: `Максимальная длина почты - ${emailMaxLength} символов!`
+                  }
+                })}/>
+            </Box>
 
-          <Box class="sign-up-field-error-box centered">
-            {
-              errors?.username &&
-              <p class="sign-up-field-error-message">
-                {errors?.username?.message || "Такой юзернейм уже занят :("}
-              </p>
-            }
-          </Box>
+            <Box class="sign-up-field-error-box centered">
+              {
+                errors?.email &&
+                <p class="sign-up-field-error-message">
+                  {errors?.email?.message || "Ошибка валидации почты!"}
+                </p>
+              }
+            </Box>
 
-          <Box class="sign-up-field-box centered">
-            <TextField size="small" fullWidth id="password" label="Пароль" type="password"
-              variant="outlined" color="secondary" {...register("password", {
-                required: "Обязательно введите пароль!",
-                minLength: {
-                  value: passwordMinLength,
-                  message: `Минимальная длина пароля - ${passwordMinLength} символов!`
-                },
-                maxLength: {
-                  value: passwordMaxLength,
-                  message: `Максимальная длина пароля - ${passwordMaxLength} символов!`
-                }
-              })}/>
-          </Box>
+            <Box class="sign-up-field-box centered">
+              <TextField size="small" fullWidth id="username" label="Юзернейм"
+                variant="outlined" color="secondary" {...register("username", {
+                  required: "Обязательно введите юзернейм!",
+                  validate: value => isUsernameUnique(value),
+                  pattern: {
+                    value: /^[a-z0-9_-]*$/,
+                    message: "Введите корректный юзернейм!"
+                  },
+                  minLength: {
+                    value: usernameMinLength,
+                    message: `Минимальная длина юзернейма - ${usernameMinLength} символов!`
+                  },
+                  maxLength: {
+                    value: usernameMaxLength,
+                    message: `Максимальная длина юзернейма - ${usernameMaxLength} символов!`
+                  }
+                })}/>
+            </Box>
 
-          <Box class="sign-up-field-error-box centered">
-            {
-              errors?.password &&
-              <p class="sign-up-field-error-message">
-                {errors?.password?.message || "Ошибка валидации пароля!"}
-              </p>
-            }
-          </Box>
+            <Box class="sign-up-field-error-box centered">
+              {
+                errors?.username &&
+                <p class="sign-up-field-error-message">
+                  {errors?.username?.message || "Такой юзернейм уже занят :("}
+                </p>
+              }
+            </Box>
 
-          <Box class="centered">
-            <Button variant="contained" 
-              color="secondary" type="submit" disabled={!isValid}>
-              Зарегистрироваться!
-            </Button>
+            <Box class="sign-up-field-box centered">
+              <TextField size="small" fullWidth id="password" label="Пароль" type="password"
+                variant="outlined" color="secondary" {...register("password", {
+                  required: "Обязательно введите пароль!",
+                  minLength: {
+                    value: passwordMinLength,
+                    message: `Минимальная длина пароля - ${passwordMinLength} символов!`
+                  },
+                  maxLength: {
+                    value: passwordMaxLength,
+                    message: `Максимальная длина пароля - ${passwordMaxLength} символов!`
+                  }
+                })}/>
+            </Box>
+
+            <Box class="sign-up-field-error-box centered">
+              {
+                errors?.password &&
+                <p class="sign-up-field-error-message">
+                  {errors?.password?.message || "Ошибка валидации пароля!"}
+                </p>
+              }
+            </Box>
+
+            <Box>
+              <Button id="sign-up-button" variant="contained" 
+                color="secondary" type="submit" disabled={!isValid}>
+                Зарегистрироваться!
+              </Button>
+            </Box>
+          </form>
         </Box>
-        </form>
       </Box>
     </div>
   );
